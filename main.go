3f2c1ad2e50bff0975103e09069bff97825a9255
EdwinWalela <edwinwalela@gmail.com>
2021-07-09 19:45:03 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/edwinwalela/jamii-bio/routes"
+	"github.com/gorilla/mux"
+)
+
+const PORT = 8000
+
+var URL = fmt.Sprintf("0.0.0.0:%d", PORT)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/detect", routes.DetectHandler)
+	r.HandleFunc("/verify", routes.VerificationHandler)
+
+	srv := &http.Server{
+		Addr:         URL,
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      r,
+	}
+
+	log.Printf("Listening for requests on port:%d\n", PORT)
+	log.Fatal(srv.ListenAndServe())
+
+}
---
+++ routes/face.go
@@ -0,0 +1,11 @@
+package routes
+
+import "net/http"
+
+func DetectHandler(w http.ResponseWriter, r *http.Request) {
+	return
+}
+
+func VerificationHandler(w http.ResponseWriter, r *http.Request) {
+	return
+}
