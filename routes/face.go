6d5693465bcbd336d6b34470850076c36ae8b68b
EdwinWalela <edwinwalela@gmail.com>
2021-07-09 23:24:08 +0300

    retain image file extension

--- routes/face.go
+++ routes/face.go
@@ -5,30 +5,40 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 const UploadPath = "/static/images/"
 
 func DetectHandler(w http.ResponseWriter, r *http.Request) {
-	idFace, idFaceHandler, idErr := r.FormFile("id")
-	// userFace, userFaceHandler, faceErr := r.FormFile("face")
+	idFace, idFormFile, idErr := r.FormFile("id")
+	userFace, faceFormFile, faceErr := r.FormFile("face")
+
+	idFaceFileName := "id-1234." + strings.Split(idFormFile.Filename, ".")[1]
+	userFaceFileName := "face-1234." + strings.Split(faceFormFile.Filename, ".")[1]
 
 	if idErr != nil {
 		panic(idErr)
 	}
-	// if faceErr != nil {
-	// 	panic(faceErr)
-	// }
+	if faceErr != nil {
+		panic(faceErr)
+	}
 
 	defer idFace.Close()
-	// defer userFace.Close()
-	path := filepath.Join(".", UploadPath)
-	idFile, err := os.OpenFile(path+idFaceHandler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	defer userFace.Close()
+
+	idPath := filepath.Join(".", UploadPath, idFaceFileName)
+	facePath := filepath.Join(".", UploadPath, userFaceFileName)
+
+	idFile, err := os.OpenFile(idPath, os.O_WRONLY|os.O_CREATE, 0666)
+	faceFile, err := os.OpenFile(facePath, os.O_WRONLY|os.O_CREATE, 0666)
+
 	if err != nil {
 		panic(err)
 	}
 
 	defer idFile.Close()
+	defer faceFile.Close()
 
 	_, _ = io.WriteString(w, "File uploaded")
 	_, _ = io.Copy(idFile, idFace)
