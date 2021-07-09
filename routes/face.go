2b2e57a8264aa247feeb6cbd8e6b0e224ba88b56
EdwinWalela <edwinwalela@gmail.com>
2021-07-09 19:58:47 +0300

    upload file

--- routes/face.go
+++ routes/face.go
@@ -1,9 +1,35 @@
 package routes
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+	"os"
+)
 
 func DetectHandler(w http.ResponseWriter, r *http.Request) {
-	return
+	idFace, idFaceHandler, idErr := r.FormFile("id")
+	// userFace, userFaceHandler, faceErr := r.FormFile("face")
+
+	if idErr != nil {
+		panic(idErr)
+	}
+	// if faceErr != nil {
+	// 	panic(faceErr)
+	// }
+
+	defer idFace.Close()
+	// defer userFace.Close()
+
+	idFile, err := os.OpenFile("id", os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	defer idFile.Close()
+
+	_, _ = io.WriteString(w, "File uploaded")
+	_, _ = io.Copy(idFile, idFace)
+
 }
 
 func VerificationHandler(w http.ResponseWriter, r *http.Request) {
